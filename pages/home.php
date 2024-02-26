dc2f0ac5097e2ae4ba8fea795845d2aaeb96bbb1
Leonardo <8293569@itisrossi.vi.it>
2024-02-26 13:29:26 +0100

    prodotti

--- pages/home.php
+++ pages/home.php
@@ -4,8 +4,27 @@
     <meta charset="UTF-16">
     <title>Home</title>
     <link rel="stylesheet" type="text/css" href="../style/style.css">
+    <?php
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'bar';
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    ?>
 </head>
 <body>
-
+<form action="home.php" method="post">
+    <label>Piatto:
+        <select>
+            <?php
+            $query = "SELECT * FROM prodotto;";
+            $products = $conn->query($query);
+            while ($p = $products->fetch_assoc())
+                echo "<option value='".$p['id']."'>".$p['nome']." €".$p['prezzo']."</option>";
+            ?>
+        </select>
+    </label>
+</form>
 </body>
 </html>

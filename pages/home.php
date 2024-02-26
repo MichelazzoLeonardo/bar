b7db1fc0c47c33cc9c9c7df4314f555888138a84
Leonardo <8293569@itisrossi.vi.it>
2024-02-26 13:10:52 +0100

    homepage base

---
+++ pages/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-16">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+</head>
+<body>
+
+</body>
+</html>

c4dcfbbde111ce55236393f5e0ce338f28aac102
Leonardo <8293569@itisrossi.vi.it>
2024-03-04 09:16:13 +0100

    ajax

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-16">
+    <title>AJAX</title>
+    <script>
+        function showHint(str) {
+            document.getElementById("txtHint").textContent = str;
+        }
+
+    </script>
+</head>
+<body>
+<p><b>Start typing a name in the input field below:</b></p>
+<form action="">
+    <label for="fname">First name:</label>
+    <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+</form>
+<p>Suggestions: <span id="txtHint"></span></p>
+</body>
+</html>
